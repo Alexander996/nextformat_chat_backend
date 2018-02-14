84b7e90817ea19a24e4bafab89354a228f33e9fb
Alexander Smirnov <al.smirnov996@gmail.com>
2018-02-14 12:46:55 +0300

    Create models

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Chat(models.Model):
+    name = models.CharField(max_length=30)
+    users = models.ManyToManyField(User)
+
+
+class Message(models.Model):
+    auto_date = models.DateTimeField(auto_now_add=True)
+    chat = models.ForeignKey(Chat, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    text = models.TextField()
